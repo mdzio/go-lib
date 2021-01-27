426e23b1be538338b9be2473028933480209dc4a
mdzio <info@ccu-historian.de>
2021-01-27 19:16:30 +0100

    added testutil

---
+++ testutil/testutil.go
@@ -0,0 +1,32 @@
+package testutil
+
+import (
+	"os"
+	"testing"
+
+	"github.com/mdzio/go-logging"
+)
+
+// Test configuration (environment variables)
+const (
+	// log level, e.g. OFF, ERROR, WARNING, INFO, DEBUG, TRACE
+	logLevel = "LOG_LEVEL"
+)
+
+func init() {
+	var l logging.LogLevel
+	err := l.Set(os.Getenv(logLevel))
+	if err == nil {
+		logging.SetLevel(l)
+	}
+}
+
+// Config reads a test configuration. Test is skipped, if the environment
+// variable with the specified name is not found.
+func Config(t *testing.T, name string) string {
+	v := os.Getenv(name)
+	if len(v) == 0 {
+		t.Skip("Environment variable " + name + " not set")
+	}
+	return v
+}
