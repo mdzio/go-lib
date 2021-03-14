478a2dc0da0235516b0b7ed7b6f4557ff90f30f6
mdzio <info@ccu-historian.de>
2021-03-14 12:22:43 +0100

    marginal optimization

--- conc/daemon.go
+++ conc/daemon.go
@@ -3,7 +3,9 @@ package conc
 // DaemonFunc runs the specified function as daemon. Calling the returned function
 // signals that the function should be canceled.
 func DaemonFunc(f func(Context)) func() {
-	term := make(chan struct{})
+	// size of 1 does not block go routine, if the function is completed before
+	// it is cancelled.
+	term := make(chan struct{}, 1)
 	ctx := &context{
 		done: make(chan struct{}),
 	}
--- conc/daemon_test.go
+++ conc/daemon_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func TestFunc(t *testing.T) {
+func TestDaemonFunc(t *testing.T) {
 	l := []int{}
 	c := DaemonFunc(func(ctx Context) {
 		l = append(l, 1)
@@ -18,6 +18,24 @@ func TestFunc(t *testing.T) {
 	})
 	c()
 	if !reflect.DeepEqual(l, []int{1, 2}) {
+		t.Fatal(l)
+	}
+
+	l = []int{}
+	w := make(chan struct{})
+	c = DaemonFunc(func(ctx Context) {
+		l = append(l, 1)
+		if err := ctx.Sleep(0); err == ErrCanceled {
+			l = append(l, 2)
+			return
+		}
+		l = append(l, 3)
+		w <- struct{}{}
+	})
+	<-w
+	time.Sleep(100 * time.Millisecond)
+	c()
+	if !reflect.DeepEqual(l, []int{1, 3}) {
 		t.Fatal(l)
 	}
 }
